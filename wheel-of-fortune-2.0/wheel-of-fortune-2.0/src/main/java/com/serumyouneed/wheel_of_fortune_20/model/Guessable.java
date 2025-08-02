461f4e7b164cc0e96ca13b9bab66e1c7dee9c9c5
Greg Miler <greg.miler@gmail.com>
2025-08-02 23:46:25 +0200

    Add Wheel-Of-Fortune-refactor project

---
+++ wheel-of-fortune-2.0/wheel-of-fortune-2.0/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/Guessable.java
@@ -0,0 +1,5 @@
+package com.serumyouneed.wheel_of_fortune_20.model;
+
+public interface Guessable {
+    String getText();
+}
---
+++ wheel-of-fortune-2.0/wheel-of-fortune-2.0/src/test/java/com/serumyouneed/wheel_of_fortune_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serumyouneed.wheel_of_fortune_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
