a14c05a1b44c6d84a5f6a9ea2c36e9595815b3ea
Greg Miler <greg.miler@gmail.com>
2025-08-03 23:51:21 +0200

    Add Wheel-Of-Fortune-refactor project

--- wheel-of-fortune-2.0/wheel-of-fortune-2.0/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/Movie.java
+++ wheel-of-fortune-2.0/wheel-of-fortune-2.0/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/Movie.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Table;
  *
  */
 @Entity
-@Table(name = "movies")
+@Table(name = "mo")
 public class Movie implements Guessable{
     private final int id;
     private final String title;
