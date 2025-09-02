8a3782810fdeacfda765465bac15e25536b58518
Greg Miler <greg.miler@gmail.com>
2025-09-02 23:58:03 +0200

    Add Wheel-Of-Fortune-2.0 project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,3 @@
 import { useState } from 'react'
 import './App.css'
-
-function App() {
-
-  return (
-    <>
-     <WelcomeHeader />
-     <Menu />
-     <button>Log in</button>
-     <button>Log in</button>
-     <button>Log in</button>
-    </>
-  )
-}
-
-export default App
-
-function WelcomeHeader() {
-    return <h1>Welcome to "Wheel of fortune"!</h1>
-}
-
-function Menu() {
-    return
-     <ul>
-         <li>Log in</li>
-         <li>Sign in</li>
-         <li>Play as guest</li>
-         <li>delete</li>
-     </ul>
-
-    }
+import {Nav} from './Nav.jsx'
