76c17b377c51513e7371e2b35c19bb816c915ebb
Greg Miler <greg.miler@gmail.com>
2025-12-03 19:41:10 +0100

    Add Wheel-Of-Fortune-2.0 project

--- backend/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/RoomState.java
+++ backend/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/RoomState.java
@@ -3,17 +3,10 @@ package com.serumyouneed.wheel_of_fortune_20.model;
 import java.util.*;
 
 public class RoomState {
+    private final List<PlayerState> playerStates = new ArrayList<>();
+    private int currentPlayerIndex = 0;
+    private String puzzle;
+    private Set<Character> usedLetters = new HashSet<>();
 
-    public Collection<PlayerState> getPlayerStates() {
-        return ;
-    }
-
-    public class RoomState {
-        private final List<PlayerState> playerStates = new ArrayList<>();
-        private int currentPlayerIndex = 0;
-        private String puzzle;
-        private Set<Character> usedLetters = new HashSet<>();
-
-        public List<PlayerState> getPlayerStates() { return playerStates; }
-    }
+    public List<PlayerState> getPlayerStates() { return playerStates; }
 }
