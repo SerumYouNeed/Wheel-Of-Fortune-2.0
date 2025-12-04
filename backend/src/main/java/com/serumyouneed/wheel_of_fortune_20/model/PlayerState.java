1f043b708f754fe917f74eab086dac2e1ca2e625
Greg Miler <greg.miler@gmail.com>
2025-12-04 19:44:40 +0100

    Add Wheel-Of-Fortune-2.0 project

--- backend/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/PlayerState.java
+++ backend/src/main/java/com/serumyouneed/wheel_of_fortune_20/model/PlayerState.java
@@ -1,7 +1,7 @@
 package com.serumyouneed.wheel_of_fortune_20.model;
 
 public class PlayerState {
-    private final String playerId; // UUID lub nickname
+    private final String playerId;
     private int points;
     private boolean turn;
     private char lastSpinLetter;
